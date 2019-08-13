f8ab612adb930d682f87db93c4469e6737112a5b
Harsh Pancholi <harshpancholi8@gmail.com>
2019-08-13 17:49:49 +0530

    Create upload directory on start of server

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 require('dotenv').config()
+// dependencies
 const mongo = require('mongodb').MongoClient
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const cors = require('cors');
+const mkdir = require('make-dir');
+
 
 global.appRoot = path.resolve(__dirname)
 
+// initialize express
 const app = express();
 
 
@@ -16,8 +20,10 @@ const fetchData = require('./routes/fetch');
 const listFile = require('./routes/listFiles');
 const bindClient = require('./routes/event').bindClient;
 
+// env variables
 const url = `${process.env.DB_URL}:${process.env.DB_PORT}`
 const port = process.env.APP_PORT;
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(function (req, res, next) {
@@ -25,7 +31,7 @@ app.use(function (req, res, next) {
         'Access-Control-Allow-Origin', '*'
     );
     next();
-})
+});
 
 app.post('/upload', uploadFile);
 
@@ -45,5 +51,17 @@ mongo.connect(url, (err, client) => {
     // share mongo clinet to manage all requests
     app.locals.collections = client.db('forecast-data');
     app.listen(port, () => console.log("server started at port " + port));
+    // create a directory to store uploaded files
+    (async () => {
 
+        try {
+            await mkdir('uploaded_files');
+            console.log("Directory created successfully");
+        }
+        catch (e) {
+            console.log("Error in creating directory");
+            console.error(e)
+        }
+
+    })();
 });
